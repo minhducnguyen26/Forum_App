f22cf5ba86c0a1d87138f66cf1b67a797a30aa7c
minhducnguyen26 <minhducnguyen260101@gmail.com>
2021-07-12 19:45:51 -0600

    Final Touch - Ready to Deploy

--- server.js
+++ server.js
@@ -17,7 +17,7 @@ if (process.env.NODE_ENV === "production") {
 
 //? Cors
 const cors = require("cors");
-app.use(cors());
+app.use(cors( {origin: 'http://forum2021.codeschool.cloud'} ));
 
 //? Thread model
 const Thread = require("./model");
