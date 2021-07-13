67fd05542bbbafba54bef4597c101887734112cf
minhducnguyen26 <minhducnguyen260101@gmail.com>
2021-07-12 19:32:56 -0600

    Final Touch - Ready to Deploy

--- client/src/pages/Home.vue
+++ client/src/pages/Home.vue
@@ -113,7 +113,7 @@ export default {
   data() {
     return{
       // main url
-      url: "http://forum2021.codeschool.cloud",
+      url: "https://forum2021.codeschool.cloud",
 
       // category options
       categories: [
